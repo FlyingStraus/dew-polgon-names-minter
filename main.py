14b4017b89c5b9bdcf118dc24c23a2bd7220ffb6
FlyingStraus <105660597+FlyingStraus@users.noreply.github.com>
2023-07-21 06:43:42 +0600

    Fully finished the code

--- main.py
+++ main.py
@@ -18,22 +18,28 @@ headers = {"Content-Type": "application/json",
 send_url = "https://api.dew.gg/uniapi/domains/dew/records/sign_mint_order"
 
 
-private_key = "dbddd9b01d20309f5f63bab37298fd8ab73078bf4c5e9bb901b4e4861b2d6a01"
+# private_key = "dbddd9b01d20309f5f63bab37298fd8ab73078bf4c5e9bb901b4e4861b2d6a01"
+
+wallets = ["dbddd9b01d20309f5f63bab37298fd8ab73078bf4c5e9bb901b4e4861b2d6a01","4028fe89d7e91d7f6457bd5442760d9c7bf19c506686548f054a208068272a9a"]
 
 
 contract_address = '0xc9ae29bb6df1545fd14f282fc756c2cc8217ed59'
 value = 0
 
-names = ["ih"]
+names = ["ih","5j","6j"]
 
-async def mint(data):
+start = time.time()
+
+mint_start_time = 1689900180
+
+async def mint(data,private_key):
 
     transaction = {
         "to": w3.to_checksum_address(contract_address),
         "value": w3.to_wei(value, "ether"),
         "gas": 300000,  # Adjust gas according to your requirements
         "gasPrice": w3.to_wei("130", "gwei"),  # Adjust gas price according to your requirements
-        "nonce": await w3.eth.get_transaction_count(w3.to_checksum_address(w3.eth.account.from_key(private_key).address)),
+        "nonce": (await w3.eth.get_transaction_count(w3.to_checksum_address(w3.eth.account.from_key(private_key).address))),
         "chainId": 137,
         "data": data,
     }
@@ -42,6 +48,7 @@ async def mint(data):
 
     # Send the transaction
     tx_hash = await w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+    print(time.time()-start)
     tx_receipt = await w3.eth.wait_for_transaction_receipt(tx_hash)
 
     return tx_receipt
@@ -53,15 +60,15 @@ def request_sender():
     async def get_all():
         async with aiohttp.ClientSession() as session:
 
-            async def fetch(name):
+            async def fetch(name,private_key):
                 json = {"name":str(name)}
                 async with session.post(send_url, json = json,headers=headers) as response:
                     data = (await response.json())["data"]["mint_call_data"]
                     print(f"{name} - {data}")
-                    return await mint(data)
+                    return await mint(data,private_key)
                     
             return await asyncio.gather(*[
-                fetch(name) for name in names
+                fetch(name,wallet) for name, wallet in zip(names, wallets)
             ])
         
 
@@ -69,5 +76,12 @@ def request_sender():
 
 
 if __name__ == '__main__':
+
+    while mint_start_time > time.time():
+        print(f"wait - {mint_start_time - time.time()}")
+        time.sleep(1)
+
+    print("Minting")
+
     um = request_sender()
     print(um)
